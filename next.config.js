08599c2de91325c4393d9cd7a41c75a2851d6d66
eee <erin.lee@candidco.com>
2019-11-25 09:25:31 -0500

    lowercase env variables

--- next.config.js
+++ next.config.js
@@ -3,9 +3,8 @@ const withCSS = require('@zeit/next-css')
 
 module.exports = withCSS({
   env: {
-    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
-    FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
-    FIREBASE_DATABASE_URL: process.env.FIREBASE_DATABASE_URL,
-    FIREBASE_PROEJECT_ID: process.env.FIREBASE_PROEJECT_ID,
+    firebase_api_key: process.env.firebase_api_key,
+    firebase_auth_domain: process.env.firebase_auth_domain,
+    firebase_database_url: process.env.firebase_database_url,
   },
 })
