3eb311cedab70072e49cb4e03aeea05519aa6190
eee <erin.lee@candidco.com>
2019-11-25 20:25:12 -0500

    remove getInitialProps from App

--- pages/_app.js
+++ pages/_app.js
@@ -4,13 +4,6 @@ import React from 'react'
 import App from 'next/app'
 
 export default class MyApp extends App {
-    static async getInitialProps(appContext) {
-    // calls page's `getInitialProps` and fills `appProps.pageProps`
-    const appProps = await App.getInitialProps(appContext);
-
-    return { ...appProps }
-  }
-
   render() {
     const { Component, pageProps } = this.props
 
