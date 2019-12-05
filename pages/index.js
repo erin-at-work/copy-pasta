d7ab105ba33f7a29fab11665c71b78b7754978c3
eee <erin.lee@candidco.com>
2019-12-05 01:11:51 -0500

    Side panel

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import Head from "next/head";
 
 import { postNewEntry } from "../lib/api";
 
-import Nav from "../components/nav";
 import Entry from "../components/entry";
 
 const Home = ({ entries }) => {
@@ -13,6 +12,7 @@ const Home = ({ entries }) => {
   const [description, setDescription] = useState("");
   const [link, setLink] = useState("");
   const [formVisible, setFormVisible] = useState(false);
+  const [panelVisible, setPanelVisible] = useState(true);
 
   useEffect(() => {
     setFilter(allEntries);
@@ -73,94 +73,103 @@ const Home = ({ entries }) => {
   };
 
   return (
-    <div className="bg-gray-200 min-h-screen h-full">
+    <div
+      className={`bg-gray-200 min-h-screen h-full ${
+        panelVisible ? "is-visible" : ""
+      }`}
+    >
       <Head>
         <title>🍝</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Nav />
+      <nav className="panel"></nav>
+      <div className="push-container">
+        <div className="flex p-5 justify-center">
+          <button onClick={() => setPanelVisible(!panelVisible)}>✂️🍝</button>
+        </div>
 
-      <div className="container mx-auto w-full max-w-lg flex flex-col pb-8 px-4">
-        <input
-          onChange={ev => handleOnSearch(ev.target.value)}
-          autoFocus
-          placeholder="Search here"
-          type="text"
-          className="bg-gray-300 mb-5 appearance-none border-2 border-gray-300 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-        />
+        <div className="container mx-auto w-full max-w-lg flex flex-col pb-8 px-4">
+          <input
+            onChange={ev => handleOnSearch(ev.target.value)}
+            autoFocus
+            placeholder="Search here"
+            type="text"
+            className="bg-gray-300 mb-5 appearance-none border-2 border-gray-300 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+          />
 
-        {formVisible ? (
-          <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-6 w-full">
-            <label
-              htmlFor="text"
-              className="block text-center text-gray-700 text-sm font-bold mb-2"
-            >
-              All Carbs, No Cardio
-            </label>
-            <form onSubmit={handleOnSubmit} id="form">
-              <textarea
-                autoFocus={formVisible}
-                onChange={ev => setContent(ev.target.value)}
-                placeholder="Say something interesting..."
-                type="text"
-                className="shadow appearance-none border-rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              />
-              <input
-                onChange={ev => setDescription(ev.target.value)}
-                placeholder="Description"
-                type="text"
-                className="shadow appearance-none border-rounded mt-5 w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              />
-              <input
-                onChange={ev => setLink(ev.target.value)}
-                placeholder="http://"
-                type="text"
-                className="shadow appearance-none border-rounded w-full mt-5 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              />
-              <button
-                type="submit"
-                className="mt-5 w-full bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          {formVisible ? (
+            <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-6 w-full">
+              <label
+                htmlFor="text"
+                className="block text-center text-gray-700 text-sm font-bold mb-2"
               >
-                Submit
-              </button>
-              <div className="m5-5 w-full text-center">
+                All Carbs, No Cardio
+              </label>
+              <form onSubmit={handleOnSubmit} id="form">
+                <textarea
+                  autoFocus={formVisible}
+                  onChange={ev => setContent(ev.target.value)}
+                  placeholder="Say something interesting..."
+                  type="text"
+                  className="shadow appearance-none border-rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                />
+                <input
+                  onChange={ev => setDescription(ev.target.value)}
+                  placeholder="Description"
+                  type="text"
+                  className="shadow appearance-none border-rounded mt-5 w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                />
+                <input
+                  onChange={ev => setLink(ev.target.value)}
+                  placeholder="http://"
+                  type="text"
+                  className="shadow appearance-none border-rounded w-full mt-5 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                />
                 <button
-                  type="button"
-                  className="mt-5 text-small text-gray-600 focus:outline-none focus:shadow-outline"
-                  onClick={() => setFormVisible(false)}
+                  type="submit"
+                  className="mt-5 w-full bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
                 >
-                  Hide
+                  Submit
                 </button>
-              </div>
-            </form>
+                <div className="m5-5 w-full text-center">
+                  <button
+                    type="button"
+                    className="mt-5 text-small text-gray-600 focus:outline-none focus:shadow-outline"
+                    onClick={() => setFormVisible(false)}
+                  >
+                    Hide
+                  </button>
+                </div>
+              </form>
+            </div>
+          ) : (
+            <div>
+              <button
+                type="button"
+                onClick={() => setFormVisible(true)}
+                className="mb-5 w-full text-white bg-teal-600 hover:bg-teal-700 py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              >
+                Add a new dish
+              </button>
+            </div>
+          )}
+          <div className="bg-white mt-9 mb-9 w-full rounded p-6 shadow-md">
+            <ul>
+              {filteredList.map(entry => (
+                <Entry
+                  entry={entry}
+                  key={entry.id}
+                  removeEntryFromList={removeEntry}
+                />
+              ))}
+            </ul>
+            {!Boolean(filteredList.length) && (
+              <p className="italic text-center text-gray-400">
+                Mamma mia! Basta pasta!
+              </p>
+            )}
           </div>
-        ) : (
-          <div>
-            <button
-              type="button"
-              onClick={() => setFormVisible(true)}
-              className="mb-5 w-full text-white bg-teal-600 hover:bg-teal-700 py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            >
-              Add a new dish
-            </button>
-          </div>
-        )}
-        <div className="bg-white mt-9 mb-9 w-full rounded p-6 shadow-md">
-          <ul>
-            {filteredList.map(entry => (
-              <Entry
-                entry={entry}
-                key={entry.id}
-                removeEntryFromList={removeEntry}
-              />
-            ))}
-          </ul>
-          {
-            !Boolean(filteredList.length) && (
-              <p className="italic text-center text-gray-400">Mamma mia! Basta pasta!</p>
-            )
-          }
         </div>
       </div>
     </div>
